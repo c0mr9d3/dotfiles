f353271ad20933f342426959a9f525bd820e6347
BATn1ck <demto22@mail.ru>
2022-10-11 02:21:02 +0300

    Added configs: qutebrowser, sway, waybar

---
+++ qutebrowser/config.py
@@ -0,0 +1,22 @@
+from qutebrowser.config.configfiles import ConfigAPI
+from qutebrowser.config.config import ConfigContainer
+config: ConfigAPI = config
+c: ConfigContainer = c
+# from qutebrowser.api import interceptor
+
+# Disable loading autoconfig
+config.load_autoconfig(False)
+
+# Autosave session when qutebrowser is being closed
+c.auto_save.session = True
+
+# Darkmode settings
+# c.colors.webpage.darkmode.policy.images = "never"
+# c.colors.webpage.darkmode.threshold.background = 100
+
+# Disable autoplay
+c.content.autoplay = False
+
+# Font
+#c.fonts.default_family = "Terminus"
+#c.fonts.default_size = "14px"
